7af206f7362ee6a6ed075309014ce2c61f65210b
codypearce <codyapearce@gmail.com>
2017-06-25 20:22:03 -0600

    add header and footer

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './shared/Header';
+import Footer from './shared/Footer';
 
 export default class App extends React.Component {
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
-      </div>);
+     <div>
+        <Header />
+        <h1>Habit App</h1>
+        <Footer />
+      </div>
+    );
   }
 }
