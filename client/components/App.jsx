d765142ad6c0452b03b10e033d2350d34ab5e520
codypearce <codyapearce@gmail.com>
2017-06-27 23:03:42 -0600

    material ui and tap event plugin

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './shared/Header';
 import Footer from './shared/Footer';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 export default class App extends React.Component {
   render() {
