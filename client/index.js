7eaccd1d010439972124c4c5f8bbb544b1821788
codypearce <codyapearce@gmail.com>
2017-06-28 03:18:16 -0600

    get styles set up and working with webpack

--- client/index.js
+++ client/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './components/Index';
 
+require("./styles/styles.css");
+
 ReactDOM.render(<Index />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,9 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-    ]
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /(\.css)$/, loaders: ['style-loader', 'css-loader'],},
+    ],
   },
   plugins: [HtmlWebpackPluginConfig]
 }
