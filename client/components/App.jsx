564a585e77c3429dc3cc0bdcf52fd25e2abbc385
codypearce <codyapearce@gmail.com>
2017-06-28 02:59:00 -0600

    floating button and icon

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import Header from './shared/Header';
-import Footer from './shared/Footer';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
+
+import Header from './shared/Header';
+import Footer from './shared/Footer';
+import AddHabit from './modals/AddHabit';
 
 
 export default class App extends React.Component {
@@ -11,6 +13,7 @@ export default class App extends React.Component {
      <div>
         <Header />
         <h1>Habit App</h1>
+        <AddHabit />
         <Footer />
       </div>
     );
---
+++ client/components/modals/AddHabit.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import Add from 'material-ui/svg-icons/content/add';
+
+export default class AddHabit extends React.Component {
+  _openModal() {
+    console.log('opened')
+  }
+  render() {
+    return (
+     <FloatingActionButton onTouchTap={() => this._openModal()}>
+      <Add />
+     </FloatingActionButton>
+    );
+  }
+}
