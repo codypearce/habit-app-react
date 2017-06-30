cec53d89953929b7143d0fb6153f4fca06adfcc3
codypearce <codyapearce@gmail.com>
2017-06-29 23:39:11 -0600

    style footer

--- client/components/shared/Footer.js
+++ client/components/shared/Footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default class Footer extends React.Component {
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Footer</h1>
+     <div className="footer">
+        <p>Copyright 2017</p>
       </div>
     );
   }
