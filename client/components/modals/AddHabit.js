d7680e79733eaf07f769e9f8df2552e370c608e2
codypearce <codyapearce@gmail.com>
2017-06-29 23:22:18 -0600

    add dialog, textfield for name

--- client/components/modals/AddHabit.js
+++ client/components/modals/AddHabit.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
+import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
+import TextField from 'material-ui/TextField';
 import Add from 'material-ui/svg-icons/content/add';
 
-export default class AddHabit extends React.Component {
-  _openModal() {
-    console.log('opened')
+export default class AddHabit extends Component {
+  constructor() {
+    super();
+    this.state = {
+      open: false,
+    };
   }
+
+  handleOpen() {
+    this.setState({open: true});
+  };
+
+  handleClose() {
+    this.setState({open: false});
+  };
   render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={() => this.handleClose()}
+      />,
+      <FlatButton
+        label="Add"
+        primary={true}
+        onTouchTap={() => this.handleClose()}
+      />,
+    ];
     return (
-     <FloatingActionButton onTouchTap={() => this._openModal()} className="addHabitBtn">
-      <Add />
-     </FloatingActionButton>
+      <div>
+       <FloatingActionButton onTouchTap={() => this.handleOpen()} className="addHabitBtn">
+        <Add />
+       </FloatingActionButton>
+       <Dialog
+         title="Add Habit"
+         actions={actions}
+         modal={false}
+         open={this.state.open}
+         onRequestClose={() => this.handleClose()}
+       >
+       <TextField floatingLabelText="Name" />
+       </Dialog>
+     </div>
     );
   }
 }
