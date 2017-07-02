5997c84effae10c1ef316111e75c2d721fc00434
codypearce <codyapearce@gmail.com>
2017-07-02 08:40:13 -0600

    install moment, generate habitblock dates using moment

--- client/components/habits/HabitList.js
+++ client/components/habits/HabitList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Habit from './Habit';
+import Moment from 'react-moment';
+import MomentMod from 'moment';
 
 export default class HabitList extends Component {
   constructor(props) {
@@ -7,6 +9,21 @@ export default class HabitList extends Component {
   }
   componentWillReceiveProps() {
     console.log(this.props.habits)
+  }
+  _renderDates() {
+    let day;
+    const dates = [1,2,3,4,5];
+    return (
+      dates.map(date => {
+        day = MomentMod().add(String(date), 'd');
+        console.log(day)
+        return (
+          <div className="col-xs-2 center-xs">
+            <Moment className="habitBlockText" format='dddd'>{day}</Moment>
+          </div>
+        )
+      })
+    )
   }
   _renderHabits() {
     return (
@@ -16,6 +33,7 @@ export default class HabitList extends Component {
     )
   }
   render() {
+    let date = new Date();
 
     return (
       <div className="col-xs-10 col-xs-offset-1">
@@ -24,24 +42,10 @@ export default class HabitList extends Component {
             <h4>Name</h4>
           </div>
           <div className="col-xs-10 row">
-            <div className="col-xs-2 center-xs">
-              <h4>Today</h4>
+            <div className="col-xs-2 center-xs middle-xs">
+              <Moment className="habitBlockText" format='dddd'>{date}</Moment>
             </div>
-            <div className="col-xs-2 center-xs">
-              <h4>Yesterday</h4>
-            </div>
-            <div className="col-xs-2 center-xs">
-              <h4>Friday</h4>
-            </div>
-            <div className="col-xs-2 center-xs">
-              <h4>Thursday</h4>
-            </div>
-            <div className="col-xs-2 center-xs">
-              <h4>Wednesday</h4>
-            </div>
-            <div className="col-xs-2 center-xs">
-              <h4>Tuesday</h4>
-            </div>
+            {this._renderDates()}
           </div>
         </div>
         {this._renderHabits()}
